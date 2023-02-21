a02b32fa2d22361e3bcafdbd0ffa8989c488bea3
YOUR USERNAME ON GITHUB <YOUR EMAIL ON GITHUB>
2023-02-22 02:12:09 +0600

    имена пока спарсить не получается

--- logic.py
+++ logic.py
@@ -1,27 +1,31 @@
-from decouple import config
-from random import randint, choice
+import re
+
+while True:
+    with open('MOCK_DATA.txt', 'r', encoding='UTF-8') as file:
+        content = file.read()
+        print("PRESS 1 FOR NAMES \nPRESS 2 FOR EMAILS\nPRESS 3 FOR FILES\nPRESS 4 FOR COLORS\nPRESS 5 TO EXIT")
+        client = int(input('write a number from 1 to 5: '))
+        fio_list = re.findall(r'^[a-zA-Z\-]+[\s a-zA-Z\'\-]\w+[\-\'A-Za-z ]', content)
+        email_list = re.findall(r'[\w\.-]+@[\w\.-]+', content)
+        file_list = re.findall(
+            r'[\w\.-]*\.[\w\.-]*(?:ppt|png|xls|avi|jpeg|txt|mpeg|mp3|doc|mov|tiff|gif|pdf)', content)
+        color_list = re.findall(r'\#[0-9a-f]{6}', content)
+
+    def opening_new_files(list_name, name='list1'):
+        with open(name, 'w', encoding='UTF=8') as file_name:
+            file_name.write(str(list_name))
+            print(list_name)
+    if client == 1:
+        opening_new_files(fio_list, name='names_list')
+    elif client == 2:
+        opening_new_files(email_list, name='emails_list')
+    elif client == 3:
+        opening_new_files(file_list, name='files_list')
+    elif client == 4:
+        opening_new_files(color_list, name='color_list')
+    elif client == 5:
+        print('the end')
+        break
 
 
-def casinoLogic():
-    budget = config('MY_MONEY', cast=int)
-    while True:
-        user_question = input('do you want to play? yes/no: ')
-        print(f'your budget is {budget}$')
-        user_input_number = (int(input('please write down a number from 1 to 30: ')))
-        user_input_money = (int(input('how much are you willing to bet? ')))
-        my_list = [randint(1, 30)]
-        generate_number = choice(my_list)
 
-        if generate_number == user_input_number and user_question == 'yes':
-            win = user_input_money * 2
-            budget += win
-            return f'generated number is: {generate_number}, you won {win}, your total cash is: {budget}'
-        elif generate_number != user_input_number and user_question == 'yes':
-            lose = user_input_money
-            budget -= lose
-            return f'generated number is: {generate_number},you lost {lose}, your total cash is: {budget}'
-        elif user_question == 'no':
-            print(f'your total: {budget}')
-            break
-        elif user_question != 'yes' and user_question != 'no':
-            return 'please write yes or no'
--- play.py
+++
@@ -1,4 +0,0 @@
-from logic import casinoLogic
-while True:
-    if __name__ == '__main__':
-        print(casinoLogic())
